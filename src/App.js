abe141c2bbb42a24825d74b5c3803c59d66383a4
Johnathan Giang <jayhgiang@gmail.com>
2021-05-27 12:47:25 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 import MetaTags from 'react-meta-tags';
 
 function App() {
