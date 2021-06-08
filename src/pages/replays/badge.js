9925d771e036f44765205a12bb6cfb4c35954842
Johnathan Giang <jayhgiang@gmail.com>
2021-06-08 07:01:26 -0700

    update image

--- src/pages/replays/badge.js
+++ src/pages/replays/badge.js
@@ -8,7 +8,7 @@ function App() {
         <meta name="fragment" content="!"/>
         <meta property="og:title" content="Sunday" data-react-helmet="true"/>
         <meta property="og:description" content="Sunday" data-react-helmet="true"/>
-        <meta property="og:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_test.jpg" data-react-helmet="true"/>
+        <meta property="og:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_no_bg.jpg" data-react-helmet="true"/>
 
         <title>Jay Test</title>
         <meta name="description" content="Web site created using create-react-app" data-react-helmet="true"/>
@@ -16,19 +16,19 @@ function App() {
         {/* <!-- Google / Search Engine Tags --> */}
         <meta itemprop="name" content="sunday" data-react-helmet="true"/>
         <meta itemprop="description" content="SUNDAY badge" data-react-helmet="true"/>
-        <meta itemprop="image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_test.jpg" data-react-helmet="true"/>
+        <meta itemprop="image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_no_bg.jpg" data-react-helmet="true"/>
 
         {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:type" content="website" data-react-helmet="true"/>
         <meta property="og:title" content="SUNDAY badge" data-react-helmet="true"/>
         <meta property="og:description" content="this is description this is description this is description this is description" data-react-helmet="true"/>
-        <meta property="og:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_test.jpg" data-react-helmet="true"/>
+        <meta property="og:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_no_bg.jpg" data-react-helmet="true"/>
 
         {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" data-react-helmet="true"/>
         <meta name="twitter:title" content="SUNDAY badge" data-react-helmet="true"/>
         <meta name="twitter:description" content="Web site created using create-react-app" data-react-helmet="true"/>
-        <meta name="twitter:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_test.jpg" data-react-helmet="true"/>
+        <meta name="twitter:image" content="https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/finisher_no_bg.jpg" data-react-helmet="true"/>
       </Helmet>
       <header className="App-header">
         Badge
